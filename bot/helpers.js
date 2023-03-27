f165de46f2ea317c51cbe48165df9155b9e68de4
whoant <vovanhoangtuan4.2@gmail.com>
2023-03-27 17:13:27 +0700

    Add config

--- bot/helpers.js
+++ bot/helpers.js
@@ -8,7 +8,7 @@ const POE_BOT = process.env.POE_BOT;
 const fetchResponse = async() => {
     let i = 0;
     while (i < 60) {
-        const messages = await fetchMessage(POE_CHATID);
+        const messages = await fetchMessage(POE_CHATID, 2);
         if (messages[0].author === 'human' && messages[1].author === POE_BOT && messages[1].state === 'complete') {
             return messages[1].text;
         }
